79b49c208d1a7780813c846874f30dc411e839b2
IbragimKhaniev <newinterny@gmail.com>
2025-12-01 00:03:52 +0300

    feat: simple home page with 'Привет, Мир!' minimal design

--- react/src/components/Home/index.jsx
+++ react/src/components/Home/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 /**
- * Для ИИ: Это заглушка. Обнови этот компонент. Здесь должен быть начальный экран приложения.
+ * Главная страница приложения.
+ * Отображает простой текст "Привет, Мир!".
  */
 export const Home = () => {
   return (
-    <div data-easytag="id1-src/components/Home/index.jsx">
-      <div>This is home page</div>
+    <div data-easytag="id1-react/src/components/Home/index.jsx">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '24px',
+        fontFamily: 'Arial, sans-serif',
+        color: '#333',
+        backgroundColor: '#f9f9f9',
+        textAlign: 'center',
+      }}>
+        Привет, Мир!
+      </div>
     </div>
   );
 };
